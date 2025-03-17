b8530fde19673d8aebbac246c888939884764c63
JJVargas14 <jjvargas14@outlook.com>
2025-03-16 20:59:06 -0500

    Se crea una lista para alamacenar los nombres de los amigos y una función que agrega los nombres que el usuario digite a la lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos =[];
+
+function agregarAmigo(){
+    let amigo = document.querySelector('#amigo');
+
+    if(amigo != ""){
+        amigos.push(amigo.value);
+        amigo.value="";
+        //Mostrar array en pantalla
+        console.log(amigos);
+    }else{
+        alert("Por favor, inserte un nombre.");
+    }
+}
