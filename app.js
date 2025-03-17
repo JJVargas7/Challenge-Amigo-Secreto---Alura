e031ecf2dd9fcf599db50eb9f693bf587f86b421
JJVargas14 <jjvargas14@outlook.com>
2025-03-16 21:25:21 -0500

    Se creo una función que limpia y agrega todos los nombres ingresados por el usuario a la lista HTML con id listaAmigos

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ let amigos =[];
 function agregarAmigo(){
     let amigo = document.querySelector('#amigo');
 
-    if(amigo != ""){
+    //Se evalua si la entrada es vacía, si no se agrega a la lista amigos.
+    if(amigo.value != ""){
         amigos.push(amigo.value);
         amigo.value="";
-        //Mostrar array en pantalla
         console.log(amigos);
+        actualizarListaEnPantalla();
     }else{
         alert("Por favor, inserte un nombre.");
     }
+}
+
+function actualizarListaEnPantalla(){
+    let listaHTML = document.querySelector('#listaAmigos');
+
+    //Se limpia la lista HTML y luego se agregan cada uno de los nombres en la lista amigos.
+    listaHTML.innerHTML="";
+    for (let i = 0; i < amigos.length; i++) {
+        let nuevoNombre = document.createElement("li");
+        nuevoNombre.textContent = amigos[i];
+        listaHTML.append(nuevoNombre);
+    }
+    
 }
